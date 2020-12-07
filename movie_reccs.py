455237f1c06089e271f7b76ba8d6f5036c749e6d
elisagemart <elisagemart@gmail.com>
2020-12-07 14:27:22 -0600

    added session var to store info on backend

--- movie_reccs.py
+++ movie_reccs.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
+app.secret_key="asdkjnah"
 
 @app.route('/')
 def index():
    return render_template('index.html')
 
-@app.route('/quiz')
+@app.route('/quiz', methods=['POST', 'GET'])
 def quiz():
-   return render_template('quiz.html')
+   if not 'ticker' in session:
+      session['ticker'] = 0
+   if request.method == 'POST':
+      session['ticker'] = session['ticker'] + int(request.form['submit_btn'])
+   return render_template('quiz.html', answer=session['ticker'])
 
 if __name__ == '__main__':
    app.run()
