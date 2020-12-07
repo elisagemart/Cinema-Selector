6e817f139da5d1aa947d7b7b2425e185db857c1e
elisagemart <elisagemart@gmail.com>
2020-12-07 13:48:42 -0600

    added quiz page

--- movie_reccs.py
+++ movie_reccs.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 
+@app.route('/quiz')
+def quiz():
+   return render_template('quiz.html')
+
 if __name__ == '__main__':
    app.run()
